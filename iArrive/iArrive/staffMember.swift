22e3bf2b7d5ca6bffe31432dae6327b3eb67cfe1
Will <wunyinwilliam@gmail.com>
2019-06-26 16:33:39 +0800

    A_LoadingViewController done

---
+++ iArrive/iArrive/staffMember.swift
@@ -0,0 +1,30 @@
+//
+//  staffMember.swift
+//  iArrive
+//
+//  Created by Lam Wun Yin on 26/6/2019.
+//  Copyright © 2019 Lam Wun Yin. All rights reserved.
+//
+
+import UIKit
+
+class staffMember {
+    
+    // MARK: Properties
+    
+    var firstName: String
+    var lastName: String
+    var jobTitle: String
+    
+    
+    // MARK: Initialization
+    
+    init?(firstName: String, lastName: String, jobTitle: String) {
+        guard (!firstName.isEmpty && !lastName.isEmpty && !jobTitle.isEmpty) else {
+            return nil
+        }
+        self.firstName = firstName
+        self.lastName = lastName
+        self.jobTitle = jobTitle
+    }
+}
