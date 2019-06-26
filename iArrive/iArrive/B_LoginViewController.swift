26c31d9a26e429a5565b78a42c8b69caa5531464
Will <wunyinwilliam@gmail.com>
2019-06-26 17:05:50 +0800

    A_LoadingViewController with font shown correctly

---
+++ iArrive/iArrive/B_LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  B_LoginViewController.swift
+//  iArrive
+//
+//  Created by Lam Wun Yin on 26/6/2019.
+//  Copyright © 2019 Lam Wun Yin. All rights reserved.
+//
+
+import UIKit
+
+class B_LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
